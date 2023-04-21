0d12c7721e6d0634b2c16a2535135b0a3134f910
Andriy-Kolokolov <koloandre22@gmail.com>
2023-04-21 18:17:45 +0200

    add remove button

--- script.js
+++ script.js
@@ -4,20 +4,31 @@ const logField = document.getElementById('log-field');
 const btnAdd = document.getElementById('btn-add');
 const btnRemove = document.getElementById('btn-remove');
 
-let shoppingList = [];
+let shoppingList = ["lemons", "beer", "juice", "cola", "bread"];
+
+for (let i = 0; i < shoppingList.length; i++) {
+    console.log(shoppingList.at(i));
+    tableList.innerHTML += `
+        <tr id="item${i + 1}">
+            <th scope="row">${i + 1}</th>
+            <td>${shoppingList.at(i)}</td>
+        </tr>
+        `
+}
 
 btnAdd.addEventListener("click", function (event) {
     event.preventDefault();
     if (inputAddItem.value.length !== 0) {
         shoppingList.push(inputAddItem.value);
         tableList.innerHTML += `
-        <tr>
+        <tr id="item-${inputAddItem.value.replace(/\s/g, '-')}">
             <th scope="row">${shoppingList.length}</th>
-            <td>test item</td>
+            <td>${inputAddItem.value}</td>
         </tr>
         `
         logColor("success");
         logField.value = "Item added";
+        inputAddItem.value = "";
     } else {
         logColor("warning");
         logField.value = "Input is empty";
@@ -25,7 +36,13 @@ btnAdd.addEventListener("click", function (event) {
 });
 
 btnRemove.addEventListener("click", function (event) {
-   event.preventDefault();
+    event.preventDefault();
+    // remove element from DOM
+    document
+        .getElementById(`item-${inputAddItem.value.replace(/\s/g, '-')}`)
+        .remove();
+    // remove item from list
+    shoppingList.splice(shoppingList.indexOf(inputAddItem.value), 1);
 
 });
 
@@ -42,3 +59,11 @@ function logColor(color) {
     }
 }
 
+// // soluzione ciclo while
+// function printShoppingList() {
+//     let i = 0;
+//     while (i < shoppingList.length) {
+//         console.log(shoppingList.at(i));
+//         i++;
+//     }
+// }
